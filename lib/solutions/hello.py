b2f891f59e55f2ae09e2cd9edb7360a82a927223
sbx_user1060 <sbx_user1060@ip-10-12-225-211.eu-west-2.compute.internal>
2018-05-31 17:11:47 +0000

    Thu May 31 17:11:47 UTC 2018

--- lib/solutions/hello.py
+++ lib/solutions/hello.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
+    if friend_name.lower() == 'john':
+        return 'Hello, {}'.format(friend_name)
+
     return 'Hello, World!'
